dfa51d6471231cf42248544b0181bf83279113a0
chloe alcorn <ca.alcorn@outlook.com>
2018-11-12 17:46:51 +0000

    amendments added

--- test.php
+++ test.php
@@ -1,6 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <?php
 
 
-echo "Hello, World";
+//echo "Hello, World";
+
+    
+$information = array(
+    "first_name" => "Chloe",
+    "last_name" => "Alcorn",
+    "age" => 24,
+    "lives" => "Dunfermline",
+    
+);
+
+echo $information["first_name"] . " " . $information["last_name"];
 
 ?>
+    
+</body>
+</html>
+
+
